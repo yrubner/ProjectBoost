33a14505ddcee95ff060d5522a365c76966cf79e
yrubner <ymrubner@gmail.com>
2024-11-03 11:20:59 +0100

    Finished lesson 11: Controlling the Player

--- player.gd
+++ player.gd
@@ -1,16 +1,13 @@
 extends Node3D
 
-var timer: float = 0.0
-var growing_number: int = 0
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:	
-	var my_number: int = 10
-	print(my_number + 1)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		growing_number += 1
-		print(growing_number)
+	if Input.is_action_pressed("ui_accept"):
+		position.y +=  delta
+		
+	if Input.is_action_pressed("ui_left"):
+		rotate_z(delta)
+		
+	if Input.is_action_pressed("ui_right"):
+		rotate_z(-delta)
